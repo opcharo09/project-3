381b3c92c963d555da100df31883c6e72c193664
unknown <oscar.charo09@gmail.com>
2019-08-10 12:40:48 -0500

    storejs change for heroku

--- client/src/store.js
+++ client/src/store.js
@@ -4,12 +4,11 @@ import rootReducer from "./Reducers"
 const initialState = {};
 const middleware = [thunk];
 const store = createStore(
-    rootReducer,
-  
+  rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
   )
 );
 export default store;
